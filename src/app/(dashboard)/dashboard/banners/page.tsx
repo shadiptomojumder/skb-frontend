194b11d9c24147fe88e41ee74c57e50480aa9e9a
shadipto mojumder <shadiptomojumder58k@gmail.com>
2025-02-25 21:50:17 +0600

    small change 10

--- src/app/(dashboard)/dashboard/banners/page.tsx
+++ src/app/(dashboard)/dashboard/banners/page.tsx
@@ -15,6 +15,7 @@ const BannerPage = () => {
                 <h2>I will create Banner Image Update From 6</h2>
                 <h2>I will create Banner Image Update From 7</h2>
                 <h2>I will create Banner Image Update From 8</h2>
+                <h2>I will create Banner Image Update From 9</h2>
             </section>
         </section>
     );
