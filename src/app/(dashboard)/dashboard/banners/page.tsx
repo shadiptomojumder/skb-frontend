a760247a42f6fd65ab4210e7527f633e2af64b39
shadipto mojumder <shadiptomojumder58k@gmail.com>
2025-02-28 21:46:08 +0600

    do some changes on some api

--- src/app/(dashboard)/dashboard/banners/page.tsx
+++ src/app/(dashboard)/dashboard/banners/page.tsx
@@ -7,7 +7,6 @@ const BannerPage = () => {
             <BannerImageSelector />
             <section>
                 <p>Here we will create Banner form</p>
-                <p>Here we will create Banner form</p>
             </section>
         </section>
     );
