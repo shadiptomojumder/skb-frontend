55925e2a70cea9081415c8dacb8e6a3d2cdcf36f
shadipto mojumder <shadiptomojumder58k@gmail.com>
2025-04-26 18:15:03 +0600

    Replace sidebar title text with a link to the homepage

--- src/components/dashboardComponents/app-sidebar.tsx
+++ src/components/dashboardComponents/app-sidebar.tsx
@@ -132,7 +132,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
     return (
         <Sidebar {...props}>
             <SidebarHeader>
-                <p className="text-center text-3xl font-bold text-black italic">SKB BEST</p>
+                <Link href="/" className="text-center text-3xl font-bold text-black italic">SKB BEST</Link>
                 <SearchForm />
             </SidebarHeader>
             <SidebarContent>
